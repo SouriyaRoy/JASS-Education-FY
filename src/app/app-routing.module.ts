180549e688dff27288b8aeaf482fb7fc0acbc484
Souriya <souriyaroy1998@gnail.com>
2021-02-28 19:23:45 +0530

    Basic components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'users', loadChildren:() => import ('./users/users.module')
+    .then(mod => mod.UsersModule)
+    //lazy loading
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
