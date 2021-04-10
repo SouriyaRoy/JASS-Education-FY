c81869ac37f0f06d7e3edb275b03af7b095b0038
Souriya Roy <souriyaroy1998@gmail.com>
2021-04-10 15:54:48 +0530

    Feed-Content on main branch

--- src/app/views/post-create/post-create.component.ts
+++ src/app/views/post-create/post-create.component.ts
@@ -16,28 +16,49 @@ export class PostCreateComponent implements OnInit {
   PostSubmit() {
 
   }
+  AssignSubmit(){
+
+  }
+  LectureSubmit(){
+
+  }
 
   post_form = new FormGroup({
     title : new FormControl(),
     description : new FormControl()
   })
+  assignment_form = new FormGroup({
+    assignment_title : new FormControl(),
+    assignment_description : new FormControl(),
+    assignment_link_1 : new FormControl(),
+    assignment_link_2 : new FormControl()
+  })
+  lecture_form = new FormGroup({
+    lecture_title : new FormControl(),
+    lecture_description : new FormControl(),
+    lecture_link_1 : new FormControl(),
+    lecture_link_2 : new FormControl()
+  })
 
   enableBox1() {
     //this.disabledBox1 = false
-    // let p = document.getElementById('assignment_hide');
-    // p.removeAttribute('hidden');
+    let p = document.getElementById('assignment_hide');
+    p.removeAttribute('hidden');
   }
   enableBox2() {
-    this.disabledBox1 = false
+    let p = document.getElementById('lecture_hide');
+    p.removeAttribute('hidden');
   }
   enableBox3() {
-    this.disabledBox1 = false
+    let p = document.getElementById('video_hide');
+    p.removeAttribute('hidden');
   }
   enableBox4() {
-    this.disabledBox1 = false
+    let p = document.getElementById('forum_hide');
+    p.removeAttribute('hidden');
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
